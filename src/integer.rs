9e17fa499c1b7d500d99154dfef5de761ae2e819
nathanfaucett <nathanfaucett@gmail.com>
2017-02-25 11:59:52 -0600

    clean up

--- src/integer.rs
+++ src/integer.rs
@@ -27,106 +27,106 @@ pub trait Integer:
     + Shl<usize, Output = Self>
     + Shr<usize, Output = Self>
 {
-    fn count_ones(self) -> u32;
-    fn count_zeros(self) -> u32;
-    fn leading_zeros(self) -> u32;
-    fn trailing_zeros(self) -> u32;
-    fn rotate_left(self, n: u32) -> Self;
-    fn rotate_right(self, n: u32) -> Self;
-    fn signed_shl(self, n: u32) -> Self;
-    fn signed_shr(self, n: u32) -> Self;
-    fn integer_shl(self, n: u32) -> Self;
-    fn integer_shr(self, n: u32) -> Self;
-    fn swap_bytes(self) -> Self;
-    fn from_be(x: Self) -> Self;
-    fn from_le(x: Self) -> Self;
-    fn to_be(self) -> Self;
-    fn to_le(self) -> Self;
-    fn pow(self, exp: u32) -> Self;
-    fn gcd(self, other: Self) -> Self;
-    fn lcm(self, other: Self) -> Self;
-    fn is_multiple_of(self, other: Self) -> bool;
-    fn is_even(self) -> bool;
-    fn is_odd(self) -> bool;
+    fn count_ones(&self) -> u32;
+    fn count_zeros(&self) -> u32;
+    fn leading_zeros(&self) -> u32;
+    fn trailing_zeros(&self) -> u32;
+    fn rotate_left(&self, n: u32) -> Self;
+    fn rotate_right(&self, n: u32) -> Self;
+    fn signed_shl(&self, n: u32) -> Self;
+    fn signed_shr(&self, n: u32) -> Self;
+    fn integer_shl(&self, n: u32) -> Self;
+    fn integer_shr(&self, n: u32) -> Self;
+    fn swap_bytes(&self) -> Self;
+    fn from_be(x: &Self) -> Self;
+    fn from_le(x: &Self) -> Self;
+    fn to_be(&self) -> Self;
+    fn to_le(&self) -> Self;
+    fn pow(&self, exp: u32) -> Self;
+    fn gcd(&self, other: &Self) -> Self;
+    fn lcm(&self, other: &Self) -> Self;
+    fn is_multiple_of(&self, other: &Self) -> bool;
+    fn is_even(&self) -> bool;
+    fn is_odd(&self) -> bool;
 }
 
 
 macro_rules! impl_core {
     ($T:ty, $S:ty, $U:ty) => {
         #[inline]
-        fn count_ones(self) -> u32 {
-            <$T>::count_ones(self)
+        fn count_ones(&self) -> u32 {
+            <$T>::count_ones(*self)
         }
         #[inline]
-        fn count_zeros(self) -> u32 {
-            <$T>::count_zeros(self)
+        fn count_zeros(&self) -> u32 {
+            <$T>::count_zeros(*self)
         }
         #[inline]
-        fn leading_zeros(self) -> u32 {
-            <$T>::leading_zeros(self)
+        fn leading_zeros(&self) -> u32 {
+            <$T>::leading_zeros(*self)
         }
         #[inline]
-        fn trailing_zeros(self) -> u32 {
-            <$T>::trailing_zeros(self)
+        fn trailing_zeros(&self) -> u32 {
+            <$T>::trailing_zeros(*self)
         }
         #[inline]
-        fn rotate_left(self, n: u32) -> Self {
-            <$T>::rotate_left(self, n)
+        fn rotate_left(&self, n: u32) -> Self {
+            <$T>::rotate_left(*self, n)
         }
         #[inline]
-        fn rotate_right(self, n: u32) -> Self {
-            <$T>::rotate_right(self, n)
+        fn rotate_right(&self, n: u32) -> Self {
+            <$T>::rotate_right(*self, n)
         }
         #[inline]
-        fn signed_shl(self, n: u32) -> Self {
-            ((self as $S) << n) as $T
+        fn signed_shl(&self, n: u32) -> Self {
+            ((*self as $S) << n) as $T
         }
         #[inline]
-        fn signed_shr(self, n: u32) -> Self {
-            ((self as $S) >> n) as $T
+        fn signed_shr(&self, n: u32) -> Self {
+            ((*self as $S) >> n) as $T
         }
         #[inline]
-        fn integer_shl(self, n: u32) -> Self {
-            ((self as $U) << n) as $T
+        fn integer_shl(&self, n: u32) -> Self {
+            ((*self as $U) << n) as $T
         }
         #[inline]
-        fn integer_shr(self, n: u32) -> Self {
-            ((self as $U) >> n) as $T
+        fn integer_shr(&self, n: u32) -> Self {
+            ((*self as $U) >> n) as $T
         }
         #[inline]
-        fn swap_bytes(self) -> Self {
-            <$T>::swap_bytes(self)
+        fn swap_bytes(&self) -> Self {
+            <$T>::swap_bytes(*self)
         }
         #[inline]
-        fn from_be(x: Self) -> Self {
-            <$T>::from_be(x)
+        fn from_be(x: &Self) -> Self {
+            <$T>::from_be(*x)
         }
         #[inline]
-        fn from_le(x: Self) -> Self {
-            <$T>::from_le(x)
+        fn from_le(x: &Self) -> Self {
+            <$T>::from_le(*x)
         }
         #[inline]
-        fn to_be(self) -> Self {
-            <$T>::to_be(self)
+        fn to_be(&self) -> Self {
+            <$T>::to_be(*self)
         }
         #[inline]
-        fn to_le(self) -> Self {
-            <$T>::to_le(self)
+        fn to_le(&self) -> Self {
+            <$T>::to_le(*self)
         }
         #[inline]
-        fn pow(self, exp: u32) -> Self {
-            <$T>::pow(self, exp)
+        fn pow(&self, exp: u32) -> Self {
+            <$T>::pow(*self, exp)
         }
         /// # Examples
         /// ~~~
         /// use integer::Integer;
         ///
-        /// assert_eq!(9.is_multiple_of(3), true);
-        /// assert_eq!(3.is_multiple_of(9), false);
+        /// assert_eq!(9.is_multiple_of(&3), true);
+        /// assert_eq!(3.is_multiple_of(&9), false);
         /// ~~~
         #[inline]
-        fn is_multiple_of(self, other: Self) -> bool {
-            self % other == 0
+        fn is_multiple_of(&self, other: &Self) -> bool {
+            *self % *other == 0
         }
         /// # Examples
         /// ~~~
@@ -136,8 +136,8 @@ macro_rules! impl_core {
         /// assert_eq!(4.is_even(), true);
         /// ~~~
         #[inline]
-        fn is_even(self) -> bool {
-            (self) & 1 == 0
+        fn is_even(&self) -> bool {
+            (*self) & 1 == 0
         }
         /// # Examples
         /// ~~~
@@ -147,7 +147,7 @@ macro_rules! impl_core {
         /// assert_eq!(4.is_odd(), false);
         /// ~~~
         #[inline]
-        fn is_odd(self) -> bool {
+        fn is_odd(&self) -> bool {
             !self.is_even()
         }
     }
@@ -159,13 +159,13 @@ macro_rules! impl_gcd_lcd {
         /// ~~~
         /// use integer::Integer;
         ///
-        /// assert_eq!(6usize.gcd(8usize), 2usize);
-        /// assert_eq!(7usize.gcd(3usize), 1usize);
+        /// assert_eq!(6usize.gcd(&8usize), 2usize);
+        /// assert_eq!(7usize.gcd(&3usize), 1usize);
         /// ~~~
         #[inline]
-        fn gcd(self, other: Self) -> Self {
-            let mut m = self;
-            let mut n = other;
+        fn gcd(&self, other: &Self) -> Self {
+            let mut m = *self;
+            let mut n = *other;
 
             if m == 0 || n == 0 {
                 m | n
@@ -195,12 +195,12 @@ macro_rules! impl_gcd_lcd {
         /// ~~~
         /// use integer::Integer;
         ///
-        /// assert_eq!(7usize.lcm(3usize), 21usize);
-        /// assert_eq!(2usize.lcm(4usize), 4usize);
+        /// assert_eq!(7usize.lcm(&3usize), 21usize);
+        /// assert_eq!(2usize.lcm(&4usize), 4usize);
         /// ~~~
         #[inline]
-        fn lcm(self, other: Self) -> Self {
-            self * (other / self.gcd(other))
+        fn lcm(&self, other: &Self) -> Self {
+            *self * (*other / self.gcd(other))
         }
     );
     ($T:ty, signed) => (
@@ -208,13 +208,13 @@ macro_rules! impl_gcd_lcd {
         /// ~~~
         /// use integer::Integer;
         ///
-        /// assert_eq!(6.gcd(8), 2);
-        /// assert_eq!(7.gcd(3), 1);
+        /// assert_eq!(6.gcd(&8), 2);
+        /// assert_eq!(7.gcd(&3), 1);
         /// ~~~
         #[inline]
-        fn gcd(self, other: Self) -> Self {
-            let mut m = self;
-            let mut n = other;
+        fn gcd(&self, other: &Self) -> Self {
+            let mut m = *self;
+            let mut n = *other;
 
             if m == 0 || n == 0 {
                 <$T>::abs(m | n)
@@ -244,12 +244,12 @@ macro_rules! impl_gcd_lcd {
         /// ~~~
         /// use integer::Integer;
         ///
-        /// assert_eq!(7.lcm(3), 21);
-        /// assert_eq!(2.lcm(4), 4);
+        /// assert_eq!(7.lcm(&3), 21);
+        /// assert_eq!(2.lcm(&4), 4);
         /// ~~~
         #[inline]
-        fn lcm(self, other: Self) -> Self {
-            <$T>::abs(self * (other / self.gcd(other)))
+        fn lcm(&self, other: &Self) -> Self {
+            <$T>::abs(*self * (*other / self.gcd(other)))
         }
     );
 }
